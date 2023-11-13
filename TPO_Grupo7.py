2b6c118ed6eeb3b28564d4ba5213b0c7e265be25
Nehuelin <111355407+Nehuelin@users.noreply.github.com>
2023-11-12 21:53:55 -0300

    Update TPO_Grupo7.py

--- TPO_Grupo7.py
+++ TPO_Grupo7.py
@@ -1,78 +1,79 @@
 import random
-from colorama import Fore, Back, Style, init
-import time
+from colorama import Back, Style, init
 
-# USAR FUNCION RECURSIVA PARA GENERAR NUMEROS GANADORES 
-# def generar_numeros(unicos, cantidad):
-#     if cantidad == 0:
-#         return list(unicos)  # Cuando se alcanza la cantidad deseada, convertimos el conjunto en una lista y lo devolvemos.
-#     else:
-#         nuevo_numero = random.randint(0, 6)
-#         while nuevo_numero in unicos:  # Asegurarse de que el número generado sea único.
-#             nuevo_numero = random.randint(0, 6)
-#         unicos.add(nuevo_numero)
-#         return generar_numeros(unicos, cantidad - 1)
+def generar_numeros_ganadores(numeros_ganadores, cantidad):
+    if len(numeros_ganadores) == cantidad:
+        return numeros_ganadores
+    nuevo_numero = random.randint(0, 41)
+    if nuevo_numero not in numeros_ganadores:
+        numeros_ganadores.add(nuevo_numero)
+    return generar_numeros_ganadores(numeros_ganadores, cantidad)
 
-init(autoreset = True) # <-- Resetea los efectos de texto por linea (Asi no tenemos que usar Style.RESET_ALL todo el tiempo)
-ganadores = []
+def imprimir_matriz(matriz):
+    for fila in matriz:
+        for elemento in fila:
+            print("%14s" %elemento, end=" ")
+        print()
+
+def imprimir_resultados(titulo, datos):
+    print('\n' + titulo)
+    imprimir_matriz(datos)
+
+# PROGRAMA PRINCIPAL 
+init(autoreset = True)
+numeros_ganadores = generar_numeros_ganadores(set(), 6)
+    
+apostadores_ganadores = []
+lista_apostadores = []
+maximo = 0
 listado_agencias = {}
-racha_maxima = 0
-ganadores_finales = []
-
-numeros_ganadores = set()
-while len(numeros_ganadores) < 6:
-    numeros_ganadores.add(random.randint(0, 41))
 
 try:
     archivo = open("apuestas.txt", "rt")
     for linea in archivo:
-        racha_actual = 0
         aciertos = 0
-        linea = linea.rstrip("\n")
-        linea = linea.split(';')
+        linea = linea.rstrip('\n').split(';')
         numeros = linea[2:]
-        for i in range(len(numeros)):
+        
+        for i in range (len(numeros)):
             if int(numeros[i]) in numeros_ganadores:
                 aciertos += 1
-                racha_actual += 1
-        if aciertos >= 1:
-            datos = linea[0], linea[1], aciertos
-            ganadores.append(datos)   
-        if racha_actual > racha_maxima:
-            racha_maxima = racha_actual 
-        if linea[1] not in listado_agencias:
-            listado_agencias[linea[1]] = 1
-        elif linea[1] in listado_agencias:
-            listado_agencias[linea[1]] += 1  # <-- Esto funciona, pero no se va a notar al menos que el rango del random de los codigos sea entre pocos numeros
+        
+        dni, codigo_agencia = linea[:2]
+        apostador = (dni, codigo_agencia, aciertos)
+        if aciertos > 0:
+            lista_apostadores.append(apostador)
+
+        if codigo_agencia in listado_agencias:
+            listado_agencias[codigo_agencia] += 1
+        else:
+            listado_agencias[codigo_agencia] = 1
+
+        if aciertos > maximo:
+            maximo = aciertos
+            
 except FileNotFoundError as mensaje:
-    print(f"Error: {mensaje}")
+    print("ERROR:", mensaje)
 except OSError as mensaje:
-    print(f"Error: {mensaje}")
+    print("ERROR:", mensaje)
+except ValueError:
+    print("ERROR: Archivo Vacio / Formato Invalido")
 finally:
     try:
         archivo.close()
     except NameError:
         pass
+        
+print('\n' + Back.GREEN + "NUMEROS GANADORES:" + Style.RESET_ALL, str(numeros_ganadores).strip('{}').replace(',',' -'))
+print('\n' + Back.YELLOW + "CANTIDAD MAXIMA DE ACIERTOS:" + Style.RESET_ALL, maximo)
 
-print("Numeros sorteados: " + Back.RED + f"{str(numeros_ganadores).strip('{}').replace(',',' -')}")
+for i in range(len(lista_apostadores)):
+    if lista_apostadores[i][2] == maximo:
+        apostadores_ganadores.append(lista_apostadores[i])
+apostadores_ganadores = sorted(apostadores_ganadores, key = lambda x: (int(x[0]), int(x[1])))
+matriz = [list(apostador[:2]) for apostador in apostadores_ganadores]
+imprimir_resultados(Back.RED + " DNI DE GANADORES | CODIGO DE AGENCIA ", matriz)
 
-print(f"RACHA MAXIMA: {racha_maxima}")
-
-print(Style.BRIGHT + Fore.GREEN + "\nGANADORES: ")
-for i in range(len(ganadores)):
-    if ganadores[i][2] == racha_maxima:
-        ganadores_finales.append(ganadores[i])  # FUNCIONA ENCONTRAR GANADORES DE X VICTORIAS
-print(ganadores_finales) # <--- Para verificar que funciona
-ganadores_ordenados = sorted(ganadores_finales, key = lambda x: (int(x[0]), int(x[1]))) # FUNCIONA ORDENAMIENTO (AMBAS CLAVES)
-for i in range(len(ganadores_ordenados)):
-    print(Back.MAGENTA + f"DNI: {ganadores_ordenados[i][0]:2} ", f"CODIGO DE AGENCIA: {ganadores_ordenados[i][1]}")
-    
-print(Fore.LIGHTBLUE_EX + Style.BRIGHT + "\nAGENCIAS PARTICIPANTES: ")
-listado_agencias_ordenado = sorted(listado_agencias.keys(), key = lambda codigo: listado_agencias[codigo], reverse=True)
-for codigo in listado_agencias_ordenado:
-    cantidad = listado_agencias[codigo]
-    print(f"CODIGO Nro: {codigo}, CANTIDAD DE APOSTADORES: {cantidad}")
-
-# TESTING ONLY: 
-dev_total_lines = list(listado_agencias.values())
-print(f"\nDEV ONLY! Cantidad de registros: {sum(dev_total_lines)}")
+agencias = sorted(listado_agencias.keys(), key = lambda x: listado_agencias[x], reverse=True)
+matriz_agencias = [[codigo, listado_agencias[codigo]] for codigo in agencias]
+imprimir_resultados(Back.LIGHTBLUE_EX + "AGENCIAS PARTICIPANTES | CANTIDAD DE APOSTADORES", matriz_agencias)
